9eb43e954b525438fdc4b57247e25aaee5d85b64
msvanidze <mamiasvanidze@gmail.com>
2024-04-20 13:49:12 +0400

    DOCS: Starting Building Carusel

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,30 @@
+const carouselItems = document.querySelectorAll(".carousel_item");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+// It will be used to shift the items  by incrementing its
+// value every 2 seconds and multiplying it by 100
+let i = 1;
+
+// Running the condition every 2 seconds using setInterval
+setInterval(() => {
+  // Accessing All the carousel Items
+  Array.from(carouselItems).forEach((item, index) => {
+    // Checking if the value of i is less than the
+    // number of carousel items, if it is true,
+    // move all the carousel items 100% towards left
+    if (i < carouselItems.length) {
+      item.style.transform = `translateX(-${i * 100}%)`;
+    }
+  });
+
+  // If the the value of i is less than the number of
+  // Carousel items, then increment the value of i by 1
+  // Otherwise set the value of i to 0 if it becomes greater
+  // than the number of carousel items
+  if (i < carouselItems.length) {
+    i++;
+  } else {
+    i = 0;
+  }
+}, 2000);
